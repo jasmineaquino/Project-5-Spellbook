c7aa9cbbb924ef6ed6345e52d37ad532a08d27b2
jasmineaquino <51236896+jasmineaquino@users.noreply.github.com>
2019-05-31 09:36:30 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,17 @@
+//Spell #1
+
+$(".button").click(function(){
+    $(".flying").fadeToggle();
+ 
+});
+
+$("#button2").click(function() {
+    $("body").css("background-color","SpringGreen");
+
+});  
+
+
+
+
+
+
